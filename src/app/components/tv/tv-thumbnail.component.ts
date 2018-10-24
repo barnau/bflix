b1e6a44dfc7143c414a5afda494d3b934ff56e1e
unknown <blainearnau@gmail.com>
2018-10-24 15:40:17 -0700

    added tv components

---
+++ src/app/components/tv/tv-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-thumbnail',
+  templateUrl: './tv-thumbnail.component.html',
+  styleUrls: ['./tv-thumbnail.component.css']
+})
+export class TvThumbnailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
